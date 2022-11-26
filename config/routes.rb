407b328f1cd20a09a566b0204acec9ba8b7f39d9
DKReyes <dylankreyes@gmail.com>
2022-11-25 18:22:17 -0600

    Final Commit
    Added display of each specific page's layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
+  # get 'customers/index'
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "customers#index"
+
+  # get '/'  to: "customer#index"
+  get '/customers/missing_email'
+  get '/customers/alphabetized'
 end
